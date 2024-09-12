f3484b4a1c1ba5da890fc52697385919a11beab1
ZanY3 <cebureksteam@gmail.com>
2024-09-12 19:35:34 +0300

    Car select(scene loader)

--- Assets/Scripts/CarSalon/CarShop.cs
+++ Assets/Scripts/CarSalon/CarShop.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CarShop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SelectCar(int index)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(index);
     }
 }
